992137d49db1368ebad396ffef8033c4d4029884
Shish-ok <alexeyshish92@gmail.com>
2022-06-02 04:20:43 +0300

    make changeUserData

--- src/main/java/com/twowasik_project/repository/UserRepository.java
+++ src/main/java/com/twowasik_project/repository/UserRepository.java
@@ -2,9 +2,28 @@ package com.twowasik_project.repository;
 
 import com.twowasik_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String email);
 
     User findByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update users set pass = :pass where user_id = :userId", nativeQuery = true)
+    void updatePassword(@Param("pass") String newPassword, @Param("userId") int userId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE users SET user_name = :user_name WHERE user_id = :user_id", nativeQuery = true)
+    void updateUsername(@Param("user_name") String username, @Param("user_id") int userId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE users SET avatar = :avatar WHERE user_id = :user_id", nativeQuery = true)
+    void updateAvatar(@Param("avatar") String avatar, @Param("user_id") int userId);
 }
---
+++ src/main/java/com/twowasik_project/rest/ChangeController.java
@@ -0,0 +1,60 @@
+package com.twowasik_project.rest;
+
+import com.twowasik_project.dto.AuthenticationRequestDto;
+import com.twowasik_project.dto.ChangeUserDataDto;
+import com.twowasik_project.dto.JwtDto;
+import com.twowasik_project.jwt.JWTProvider;
+import com.twowasik_project.model.User;
+import com.twowasik_project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(value = "/change/")
+public class ChangeController {
+
+    @Autowired
+    JWTProvider jwtProvider;
+
+    @Autowired
+    JwtDto jwtDto;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PatchMapping("/userData")
+    public ResponseEntity changeUserData(HttpServletRequest request, @RequestBody ChangeUserDataDto changeUserDataDto) {
+
+        if (!jwtProvider.validateAccessToken(request.getHeader("Authorization"))) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User user = userRepository.findByUsername(jwtProvider.getAccessClaims(request.getHeader("Authorization")).getSubject());
+
+        String newPassword = changeUserDataDto.getNewPassword();
+        String oldPassword = changeUserDataDto.getOldPassword();
+        String newAva = changeUserDataDto.getNewAva();
+        String newUsername = changeUserDataDto.getNewUsername();
+
+        if (!newAva.equals("false")) { userRepository.updateAvatar(newAva, user.getId()); }
+
+        jwtDto.setAccessToken("false");
+        jwtDto.setRefreshToken("false");
+        if (!newUsername.equals("false")) {
+            userRepository.updateUsername(newUsername, user.getId());
+            jwtDto.setAccessToken(jwtProvider.generateAccessToken(user));
+            jwtDto.setRefreshToken(jwtProvider.generateRefreshToken(user));
+        }
+
+        if (!oldPassword.equals("false")) {
+            if (!oldPassword.equals(user.getPassword())) { return ResponseEntity.notFound().build();  }
+            userRepository.updatePassword(newPassword, user.getId());
+            changeUserDataDto.setNewPassword("true");
+        }
+
+        return ResponseEntity.ok(jwtDto);
+    }
+}
