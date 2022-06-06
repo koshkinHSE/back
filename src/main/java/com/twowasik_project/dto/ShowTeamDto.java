cc6a6f3eb6d6793e75d563b101966f8641666ba5
Shish-ok <alexeyshish92@gmail.com>
2022-06-06 17:06:41 +0300

    fix createTeam

--- src/main/java/com/twowasik_project/dto/ShowTeamDto.java
+++ src/main/java/com/twowasik_project/dto/ShowTeamDto.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Data
-@Component
+@Data @Component
 public class ShowTeamDto {
     private List<Integer> id;
     private List<String> name;
