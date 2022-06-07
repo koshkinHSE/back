c733b36342b00b076813d39b5c9dcfe4dd81da7b
Shish-ok <alexeyshish92@gmail.com>
2022-06-07 18:47:44 +0300

    delete channel

--- src/main/java/com/twowasik_project/dto/ShowChannelDto.java
+++
@@ -1,8 +0,0 @@
-package com.twowasik_project.dto;
-
-import lombok.Data;
-
-@Data
-public class ShowChannelDto {
-    private int team_id;
-}
--- src/main/java/com/twowasik_project/service/ChannelService.java
+++
@@ -1,8 +0,0 @@
-package com.twowasik_project.service;
-
-import com.twowasik_project.model.Channel;
-
-public interface ChannelService {
-
-    public Channel saveChannel(Channel channel);
-}
