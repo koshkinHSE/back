c55f9c1e144213b5a8693b18787ead47ae65cace
itskoshkin <85771670+koshkinHSE@users.noreply.github.com>
2022-06-13 20:31:55 +0300

    Fix refactor

--- src/main/java/com/twowasik/messenger/service/ChatService.java
+++ src/main/java/com/twowasik/messenger/service/ChatService.java
@@ -2,7 +2,7 @@ package com.twowasik.messenger.service;
 
 import com.twowasik.messenger.dto.LastMessageDto;
 import com.twowasik.messenger.dto.ShowDto;
-import com.corporate.model.*;
+import com.twowasik.messenger.model.*;
 import com.twowasik.messenger.model.Chat;
 import com.twowasik.messenger.model.ChatRef;
 import com.twowasik.messenger.model.Message;
